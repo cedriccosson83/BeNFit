5a3b8a75465ad49efc47f2a5a1089cd561bcfa57
cedriccosson83 <cedric.cosson@etu.univ-amu.fr>
2020-02-11 14:34:43 +0200

    Connexion Firebase

--- app/src/main/java/isen/CedricLucieFlorent/benfit/MainActivity.kt
+++ app/src/main/java/isen/CedricLucieFlorent/benfit/MainActivity.kt
@@ -2,11 +2,18 @@ package isen.CedricLucieFlorent.benfit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //lateinit var auth: FirebaseAuth
+        val database = FirebaseDatabase.getInstance()
+
+        database.getReference("users").setValue("coucou")
     }
 }
