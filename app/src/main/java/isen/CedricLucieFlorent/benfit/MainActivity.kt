4df58eea6c2fd1edad3ca7c5a21c2a9a27dcd413
cedriccosson83 <cedric.cosson@etu.univ-amu.fr>
2020-02-11 13:38:40 +0200

    FIRST COMMIT + LOGO

---
+++ app/src/main/java/isen/CedricLucieFlorent/benfit/MainActivity.kt
@@ -0,0 +1,12 @@
+package isen.CedricLucieFlorent.benfit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
