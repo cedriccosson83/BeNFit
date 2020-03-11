aea1281c5993a31784b592133419da096342628b
Luciegaire <lucie83140@hotmail.fr>
2020-03-11 13:09:08 +0100

    design notif

--- app/src/main/java/isen/CedricLucieFlorent/benfit/NotifActivity.kt
+++ app/src/main/java/isen/CedricLucieFlorent/benfit/NotifActivity.kt
@@ -8,7 +8,9 @@ import android.content.Intent
 import android.os.Build
 import android.util.Log
 import android.view.View
+import android.widget.Button
 import android.widget.Toast
+import androidx.core.view.isVisible
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
@@ -78,6 +80,11 @@ class NotifActivity : AppCompatActivity() {
 
                         inputDate.setText(previousDate)
                         inputTime.setText(previousHour)
+
+                        btnCreate.visibility = View.GONE
+                    }
+                    else{
+                    btnCancel.visibility = View.GONE
                     }
                 }
             }
